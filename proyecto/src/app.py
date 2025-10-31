d05337d3dbbc864c59ac9ffe26e472c24be4c4ec
Jkar <j.garcia861@pascualbravo.edu.co>
2025-10-30 20:02:24 -0500

    mi primer commit

--- proyecto/src/app.py
+++ proyecto/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)  # ⬅️ Cambio 1: 'flask' debe ser 'Flask'
 
@@ -10,7 +10,13 @@ def vamos():
 def hola():
     return 'hola mundo'
 
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
 if __name__ == '__main__':
     #app.run(debug=True)
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5001, debug=True)
     
+
+#Render para publicar
